0508843f58bad68d47220ca0b9c1b7e9fbcbc67d
James Bonfield <jkb@sanger.ac.uk>
2013-02-07 12:18:47 +0000

    Fixed the itf8 code again. The previous decoder change was incorrect as the encoder was at fault, not the decoder. This now seems to work correctly with -ve numbers and is interchangeable with the Java implementation.
    Replaced the cram_SAM_hdr structure with a complete bam_file_t
    structure (via typedef). This allows reuse of the bam header parsing
    code. It needs fixing properly though to make a common sub-struct used
    by both file formats.
    
    Added a (currently hacky!) bam_add_rg() function. Note it expects the
    header string to have been allocated large enough already, as realloc
    would break the pointers used in the rg_hash unless we redefine it to
    be volatile keys. The entire header implementation needs a big
    overhaul.
    
    Implemented the UNKNOWN read-group for files missing a read-group. The
    code can store read-group -1 perfectly fine, but this causes Java to
    get an array underflow so it is currently mandatory to generate an
    UNKNOWN read-group.

--- io_lib/cram_structs.h
+++ io_lib/cram_structs.h
@@ -65,14 +65,19 @@ typedef struct {
 struct cram_slice;
 
 /* SAM header */
-typedef struct {
-    uint32_t   length;
-    char      *header;
-    HashTable *ref_hash; /* Keyed on SN, value = numeric id */
-    HashTable *rg_hash;  /* Keyed on ID, value = taglist ptr */
-    uint32_t   nref;
-    bam_ref_t *ref;
-} cram_SAM_hdr;
+//typedef struct {
+//    uint32_t   length;
+//    char      *header;
+//    HashTable *ref_hash; /* Keyed on SN, value = numeric id */
+//    HashTable *rg_hash;  /* Keyed on ID, value = taglist ptr */
+//    uint32_t   nref;
+//    bam_ref_t *ref;
+//} cram_SAM_hdr;
+
+// Reuse a bam structure for our cram_SAM_hdr as it contains all the
+// necessary functions for parsing and manipulating the header already.
+// In time, merge these to be the same structure.
+typedef bam_file_t cram_SAM_hdr;
 
 enum cram_block_method {
     RAW   = 0,
