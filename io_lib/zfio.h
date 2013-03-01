1f0f631ef14f565ed5ae0171ac19baf19e3cc86d
James Bonfield <jkb@sanger.ac.uk>
2013-03-01 17:22:56 +0000

    Extended zfio.[ch] to handle writing to gzipped streams.
    Added a cram_index program to build indices. This probably ought to be
    an option during cram creation too as it's very fast (~0.4 sec vs 22
    sec from the comparable Java tool).

--- io_lib/zfio.h
+++ io_lib/zfio.h
@@ -15,6 +15,7 @@ typedef struct {
 off_t zftello(zfp *zf);
 int zfseeko(zfp *zf, off_t offset, int whence);
 char *zfgets(char *line, int size, zfp *zf);
+int zfputs(char *line, zfp *zf);
 zfp *zfopen(const char *path, const char *mode);
 int zfclose(zfp *zf);
 int zfpeek(zfp *zf);
