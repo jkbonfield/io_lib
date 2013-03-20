d9be94edc732e5da032c295caeafb561215db11d
James Bonfield <jkb@sanger.ac.uk>
2013-03-20 13:40:13 +0000

    Fixed usage statement.

--- progs/scramble.c
+++ progs/scramble.c
@@ -151,7 +151,7 @@ int main(int argc, char **argv) {
     }    
 
     if (argc - optind > 2) {
-	fprintf(stderr, "Usage: scram_convert [input_file [output_file]]\n");
+	fprintf(stderr, "Usage: scramble [input_file [output_file]]\n");
 	return 1;
     }
     
