de401735a9957713b6d84dba0db96cb26a887a4f
James Bonfield <jkb@sanger.ac.uk>
2013-10-18 11:16:37 +0000

    Added the 3-clause BSD copyright notices as issued by MRC-LMB when they made the entire package open source (which included this).
    Where possible the dates and authors are correct. They have been
    culled from semi-automatic analysis of the old MRC change.log file,
    but some files predate keeping these logs. Apologies to absent
    authors.
    
    (Next up, GRL/Sanger copyright notices.)

--- trunk/progs/extract_fastq.c
+++ trunk/progs/extract_fastq.c
@@ -1,17 +1,3 @@
-/*
- * Copyright (c) Medical Research Council 1994-1999. All rights reserved.
- *
- * Permission to use, copy, modify and distribute this software and its
- * documentation for any purpose is hereby granted without fee, provided that
- * this copyright and notice appears in all copies.
- *
- * This file was written by James Bonfield, Simon Dear, Rodger Staden,
- * as part of the Staden Package at the MRC Laboratory of Molecular
- * Biology, Hills Road, Cambridge, CB2 2QH, United Kingdom.
- *
- * MRC disclaims all warranties with regard to this software.
- */
-
 #ifdef HAVE_CONFIG_H
 #include "io_lib_config.h"
 #endif
