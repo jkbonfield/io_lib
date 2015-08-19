b9c44392e1a12ee5339567f64c649f9c78850223
James Bonfield <jkb@sanger.ac.uk>
2015-08-19 16:44:04 +0000

    Fixed bug with non-reference base CRAMs and indexing.
    When using --output-fmt cram,no_ref the reference sequence is not
    loaded into memory, causing the "ref_end" container field to be unset.
    This in turn meant that the code that clipped the container header
    start/end fields (pos+span) to the actual reference length, to work
    around buggy aligners that positiong data off the end of the
    reference, was itself buggy.

--- io_lib/cram_encode.c
+++ io_lib/cram_encode.c
@@ -2792,7 +2792,7 @@ static int process_one_read(cram_fd *fd, cram_container *c,
 	    return -1;
 	}
 	fake_qual = spos;
-	cr->aend = MIN(apos, c->ref_end);
+	cr->aend = fd->no_ref ? apos : MIN(apos, c->ref_end);
 	cram_stats_add(c->stats[DS_FN], cr->nfeature);
     } else {
 	// Unmapped
