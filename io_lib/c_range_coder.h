84aef7d8486cd0e841952967ffb9f8ccd1c07a71
James Bonfield <jkb@sanger.ac.uk>
2019-03-15 16:56:10 +0000

    Switched from 64-bit to 32-bit range coder.
    This is still based on Eugene Shelwien's coder package, but it's his
    rewrite of the 32-bit Michael Schindler version.  It's a little
    slower, but the lack of 64-bit maths as a requirement means it can be
    coded efficiently in "lesser languages", such as Javascript!

--- io_lib/c_range_coder.h
+++ io_lib/c_range_coder.h
@@ -5,7 +5,7 @@
  * Call the input() and output() functions to set and query the current
  * buffer locations.
  *
- * Derived from Eugene Shelwien's work: http://ctxmodel.net/rem-37.html.
+
  */
 
 #ifndef C_RANGER_CODER_H
@@ -13,12 +13,15 @@
 
 #define  DO(n)	   int _;for (_=0; _<n; _++)
 #define  TOP	   (1<<24)
+#define  Thres (unsigned)255*TOP
 
 typedef unsigned char uc;
 
 typedef struct {
-    uint64_t low;
-    uint32_t range, code;
+    uint32_t low, code, range;
+    uint32_t FFNum;  // Number of consecutive FFs
+    uint32_t Cache;  // Top 8-bits of low ready to emit
+    uint32_t Carry;  // Flag to indicate if we emit Cache or Cache+1
     uc *in_buf;
     uc *out_buf;
 } RangeCoder;
@@ -32,96 +35,77 @@ static inline size_t RC_InSize(RangeCoder *rc) { return rc->in_buf - rc->out_buf
 
 static inline void RC_StartEncode(RangeCoder *rc)
 { 
-    rc->low=0;  
-    rc->range=(uint32_t)-1; 
+    rc->range = 0xFFFFFFFF;
+    rc->low   = 0;
+    rc->FFNum = 0;
+    rc->Carry = 0;
+    rc->Cache = 0;
+    rc->code  = 0;
 }
 
 static inline void RC_StartDecode(RangeCoder *rc)
 { 
-    rc->code = rc->low = 0;  
-    rc->range = (uint32_t)-1;
-    DO(8) rc->code = (rc->code<<8) | *rc->in_buf++;
+    rc->range = 0xFFFFFFFF;
+    rc->low   = 0;
+    rc->FFNum = 0;
+    rc->Carry = 0;
+    rc->Cache = 0;
+    rc->code  = 0;
+    DO(5) rc->code = (rc->code<<8) | *rc->in_buf++;
+}
+
+static inline void RC_ShiftLow(RangeCoder *rc) {
+    if (rc->low < Thres || rc->Carry) {
+	*rc->out_buf++ = rc->Cache + rc->Carry;
+
+	// Flush any stored FFs
+	while (rc->FFNum) {
+	    *rc->out_buf++ = rc->Carry-1; // (Carry-1)&255;
+	    rc->FFNum--;
+	}
+
+	// Take copy of top byte ready for next flush
+	rc->Cache = rc->low >> 24;
+	rc->Carry = 0;
+    } else {
+	// Low if FFxx xxxx.  Bump FF count and shift in as before
+	rc->FFNum++;
+    }
+    rc->low = rc->low<<8;
 }
 
 static inline void RC_FinishEncode(RangeCoder *rc) 
 { 
-    DO(8) (*rc->out_buf++ = rc->low>>56), rc->low<<=8; 
+    DO(5) RC_ShiftLow(rc);
 }
 
 static inline void RC_FinishDecode(RangeCoder *rc) {}
 
-#if DIV_RCP
-static uint64_t rcp[65536];
-static inline void build_rcp_freq(void) {
-    int i;
-    for (i = 1; i < 65536; i++)
-	rcp[i] = (((uint64_t)1<<32)) / i;
-}
-#else
-static inline void build_rcp_freq(void) {}
-#endif
-
 static inline void RC_Encode (RangeCoder *rc, uint32_t cumFreq, uint32_t freq, uint32_t totFreq) 
 {
-    //fprintf(stderr, "                       RC %d+%d of %d\n", cumFreq, freq, totFreq);
-
-    // division-less doesn't help much in this case as only a single division anyway
-    //uint32_t d = (rcp[totFreq] * rc->range)>>32;
-    //d += (rc->range - totFreq * d >= totFreq);
-    //rc->low  += cumFreq * (rc->range = d);
-
+    uint32_t tmp = rc->low;
     rc->low  += cumFreq * (rc->range/= totFreq);
     rc->range*= freq;
 
-    if (cumFreq + freq > totFreq) {
-	fprintf(stderr, "cumFreq %d + freq %d > tot %d\n", cumFreq, freq, totFreq);
-	abort();
-    }
+    rc->Carry += rc->low<tmp; // Overflow
 
-    while( rc->range<TOP ) {
-	// range = 0x00ffffff..
-	// low/high may be matching
-	//       eg 88332211/88342211 (range 00010000)
-	// or differing
-	//       eg 88ff2211/89002211 (range 00010000)
-	//
-	// If the latter, we need to reduce range down
-	// such that high=88ffffff.
-	// Eg. top-1      == 00ffffff
-	//     low|top-1  == 88ffffff
-	//     ...-low    == 0000ddee
-	if ( (uc)((rc->low^(rc->low+rc->range))>>56) ) 
-	    rc->range = (((uint32_t)(rc->low)|(TOP-1))-(uint32_t)(rc->low));
-	*rc->out_buf++ = rc->low>>56, rc->range<<=8, rc->low<<=8;
+    while (rc->range < TOP) {
+	rc->range <<= 8;
+	RC_ShiftLow(rc);
     }
 }
 
 static inline uint32_t RC_GetFreq (RangeCoder *rc, uint32_t totFreq) {
-#if DIV_RCP
-    // Replacing two divisions by one is beneficial as they get stuck waiting.
-    // 2.53s
-    uint32_t d = (rcp[totFreq] * rc->range)>>32;
-    d += (rc->range - totFreq * d >= totFreq);
-    return rc->code/(rc->range=d);
-#else
-    // 2.67s
     return rc->code/(rc->range/=totFreq);
-#endif
 }
 
 static inline void RC_Decode (RangeCoder *rc, uint32_t cumFreq, uint32_t freq, uint32_t totFreq) 
 {
-    //fprintf(stderr, "                       RC %d+%d of %d\n", cumFreq, freq, totFreq);
-
-    uint32_t temp = cumFreq*rc->range;
-    rc->low  += temp;
-    rc->code -= temp;
-    rc->range*= freq;
- 
-    while( rc->range<TOP ) {
-	if ( (uc)((rc->low^(rc->low+rc->range))>>56) ) 
-	    rc->range = (((uint32_t)(rc->low)|(TOP-1))-(uint32_t)(rc->low));
-	rc->code = (rc->code<<8) | *rc->in_buf++, rc->range<<=8, rc->low<<=8;
+    rc->code -= cumFreq * rc->range;
+    rc->range *= freq;
+    while (rc->range < TOP) {
+	rc->code = (rc->code<<8) + *rc->in_buf++;
+	rc->range <<= 8;
     }
 }
 
